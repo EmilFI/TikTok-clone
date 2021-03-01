ecdd63be77a8d1f6a8c3ffd90211673bc54325a2
EmilFI <45481038+EmilFI@users.noreply.github.com>
2021-03-01 14:28:29 +0200

    Layout
    Snapping function and  basic layout

---
+++ toktik/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Video from "./Video";
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+<h1>hello</h1>
+
+<div className="app__videos">
+      <Video />
+      <Video />
+      <Video />
+      <Video />
+    </div>
+  </div>
+  );
+}
+
+export default App;
---
+++ toktik/src/Video.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Video.css';
+
+function Video() {
+    return (
+        <div className="video">
+            <h1>*video here*</h1>
+            {/* <VideoFooter /> */}
+            {/* <VideoSideBar /> /> */}
+        </div>
+    )
+}
+
+export default Video
